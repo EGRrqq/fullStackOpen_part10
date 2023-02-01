7c5ad5fe9f7f39d3a9491eb3c61757e33d7398de
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-02-01 14:49:08 +0300

    Exercise 10.13: the sign in form mutation

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,31 +1,45 @@
 import { Formik } from 'formik';
 import SignInForm from './Form';
+import useSignIn from '../../hooks/useSignIn';
 import * as yup from 'yup';
 
 const SignIn = () => {
-    const initialValues = {
-        username: '',
-        password: '',
-      };
+  const initialValues = {
+    username: '',
+    password: '',
+  };
 
-      const validationSchema = yup.object().shape({
-        username: yup
-          .string()
-          .required('username is required'),
-        password: yup
-          .string()
-          .required('password is required'),
-      });
-    
-      return (
-        <Formik
-          initialValues={initialValues}
-          onSubmit={(values) => console.log(values)}
-          validationSchema={validationSchema}
-        >
-          {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-        </Formik>
-      );
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn(username, password);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const validationSchema = yup.object().shape({
+    username: yup
+      .string()
+      .required('username is required'),
+    password: yup
+      .string()
+      .required('password is required'),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
